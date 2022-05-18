34259588fc5e404b63174078cdd2fa6c1210b680
SEKUN <sekun@hey.com>
2022-05-18 16:03:12 +0800

    feat: Add middleware for decrypting token
    JWT gets decrypted (whenever it is present in a cookie) in each request,
    automatically, and it stores the user ID in `req.current_user_id`. If
    the request does not have the token in a cookie, then the
    `req.current_user_id` is set to `null`.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 const { Pool } = require('pg');
+const auth = require('./holiplan_web/middleware/auth');
 
 let indexRouter = require('./holiplan_web/routes/index');
 let usersRouter = require('./holiplan_web/routes/users');
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(auth);
 
 app.use('/api/index', indexRouter);
 app.use('/api/users', usersRouter);
