bad6580a57174a690ec83990980fb069e76a1b78
SEKUN <sekun@hey.com>
2022-05-19 23:55:57 +0800

    fix: Remove unused route

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ let logger = require('morgan');
 const { Pool } = require('pg');
 const auth = require('./holiplan_web/middleware/auth');
 
-let indexRouter = require('./holiplan_web/routes/index');
 let usersRouter = require('./holiplan_web/routes/users');
 let plansRouter = require('./holiplan_web/routes/plans');
 let app = express();
+
 const pool = new Pool();
 
 app.set('db', pool);
@@ -19,10 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(auth);
 
-app.use('/api/index', indexRouter);
-app.use('/api/users', usersRouter);
 app.use('/api/plans', plansRouter);
-// TODO
 app.use('/api', usersRouter);
 
 module.exports = app;
